70b4ea196a68c6b8b9854f6b5d686e54cbc832b0
Kelly McGuffog <kellymcguffog@gmail.com>
2024-05-20 14:01:24 -0400

    solved countdown to midnight lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,16 @@
 # your code goes here!
+import time
+
+def countdown(x):
+    while x > 0:
+        print(f'{x} SECOND(S)!')
+        x-=1
+    print("HAPPY NEW YEAR!")
+
+
+def countdown_with_sleep(x):
+    while x > 0:
+        print(f'{x} SECOND(S)!')
+        time.sleep(1)
+        x-=1
+    print("HAPPY NEW YEAR!")
